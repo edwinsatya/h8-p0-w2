10734992a21383f793efe66c961c942cda71722c
edwinsatya <edwinsatyayudistira@gmail.com>
2019-08-01 07:29:52 +0700

    exercises-13

---
+++ exercises-13.js
@@ -0,0 +1,27 @@
+//Logic Challenge - X dan O
+
+function xo(str) {
+    // you can only write your code here!
+    var tempJmlX = 0;
+    var tempJmlO = 0;
+
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === 'x') {
+            tempJmlX++;
+        } else {
+            tempJmlO++;
+        }
+    }
+    if (tempJmlX == tempJmlO) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+  
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
